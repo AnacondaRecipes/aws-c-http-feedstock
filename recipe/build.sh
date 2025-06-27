4d803ece21cab1f724e2e1793e870486e2a1b38d
PatrykKups <pkups@anaconda.com>
2025-06-27 17:35:50 +0200

    PKG-8592: aws-c-http 0.9.3 rebuild

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,9 +10,21 @@ cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+  -DENABLE_TESTING=ON \
   ..
-ninja install
-#if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
-#  ctest -E tls_download_medium_file_h2
-#fi
+
+cmake --build . --config Release --target install
+
+if [[ ${target_platform} == osx-* ]]; then
+  # Following tests require root privileges (may prompt for root password).
+  EXCLUDE_ROOT_TESTS_APPLE="\
+connection_setup_shutdown_tls|\
+connection_customized_alpn|\
+connection_customized_alpn_error_with_unknown_return_string"
+
+  ctest -E "$EXCLUDE_ROOT_TESTS_APPLE" --output-on-failure -j${CPU_COUNT}
+else
+  ctest --output-on-failure -j${CPU_COUNT}
+fi
 popd
